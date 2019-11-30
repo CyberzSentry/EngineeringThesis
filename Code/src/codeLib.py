2d8b6cda1a813d45b44a355c45877db4d5bba93a
Maksym Brzęczek <maksym.brzeczek@gmail.com>
2019-11-30 23:13:46 +0100

    Updated the input regexes and methodes

---
+++ Code/src/codeLib.py
@@ -0,0 +1,24 @@
+
+def pesel(snip):
+    weight = [9, 7, 3, 1, 9, 7, 3, 1, 9, 7]
+    checksum = (int(snip[0]) * weight[0]) + (int(snip[1]) * weight[1]) + (int(snip[2]) * weight[2]) + (int(snip[3]) * weight[3])+ (int(snip[4]) * weight[4]) + \
+        (int(snip[5]) * weight[5]) + (int(snip[6]) * weight[6]) + (int(snip[7]) * weight[7]) + (int(snip[8]) * weight[8]) + (int(snip[9]) * weight[9])
+
+    if checksum%10 == int(snip[-1]):
+        return True
+    else:
+        return False
+
+def idnum(snip):
+    weight = [7, 3, 1, 0, 7, 3, 1, 7, 3]
+    key = {"A": 10, "B": 11, "C": 12, "D": 13, "E": 14, "F": 15, "G": 16, "H": 17, "I": 18, "J": 19, "K": 20, "L": 21, "M": 22, \
+        "N": 23, "O": 24, "P": 25, "Q": 26, "R": 27, "S": 28, "T": 29, "U": 30, "V": 31, "W": 32, "X": 33, "Y": 34, "Z": 35}
+
+    snip_ready = snip.replace(" ", "").upper()
+    checksum = (key[snip_ready[0]] * weight[0]) + (key[snip_ready[1]] * weight[1]) + (key[snip_ready[2]] * weight[2]) + (int(snip_ready[3]) * weight[3]) + \
+        (int(snip_ready[4]) * weight[4]) + (int(snip_ready[5]) * weight[5]) + (int(snip_ready[6]) * weight[6]) + (int(snip_ready[7]) * weight[7]) + (int(snip_ready[8]) * weight[8])
+
+    if checksum%10 == int(snip_ready[3]):
+        return True
+    else:
+        return False
