5220d6306d9304ffeda2e2ac9f986403f09bf456
Maksym Brzęczek <maksym.brzeczek@gmail.com>
2019-11-30 23:17:23 +0100

    Added the code imports and and initialising

--- Code/src/core.py
+++ Code/src/core.py
@@ -3,6 +3,9 @@ import json
 import os
 from threading import Thread
 import time
+import shutil
+import importlib
+from importlib import machinery
 
 class Parser:
     def __init__(self, inputPath):
@@ -10,7 +13,10 @@ class Parser:
         self.count = -1
         with open(inputPath, 'r') as inFile:
             self.inData = json.load(inFile)
+
         self.len = len(self.inData['messages'])
+        self.stepValue = int(self.len/100)
+
         self.done = False
 
     def __enter__(self):
@@ -42,55 +48,117 @@ class Core(Thread):
     
     def initInputParser(self, path):
         print("\n-----------------------Initialising input parser----------------------\n")
+        self.inputPath = path
         self.parser = Parser(path)
         pass
 
     def initDictionary(self, path):
+        print("\n------------------------Initialising dictionary-----------------------\n")
+        self.dictionaryPath = path
         pass
 
-    def initAdditional(self, additionalLibPath='regexLib.json'):
+    def initAdditional(self, additionalRegexLibPath='regexLib.json', additionalCodeLibpath='codeLib.py'):
+        """Throws key KeyError when the input file structure is incorrect"""
         print("\n--------------------Initialising additional regexes-------------------\n")
-        if additionalLibPath == 'regexLib.json':
-            additionalLibPath = os.path.join(os.path.dirname(__file__), additionalLibPath)
-        with open(additionalLibPath,'r') as rl:
+        if additionalRegexLibPath == 'regexLib.json':
+            additionalRegexLibPath = os.path.join(os.path.dirname(__file__), additionalRegexLibPath)
+
+        self.additionalRegexLibPath = additionalRegexLibPath
+
+        if additionalCodeLibpath == 'codeLib.py':
+            loader = machinery.SourceFileLoader('codeLib', os.path.join(os.path.dirname(__file__),additionalCodeLibpath))
+            self.additionalCodeModule = loader.load_module()
+            #self.codeModule = importlib.import_module(codeLibPath[:-3], os.path.dirname(__file__))
+        else:
+            loader = machinery.SourceFileLoader(os.path.basename(additionalCodeLibpath[:-3]), additionalCodeLibpath)
+            self.additionalCodeModule = loader.load_module()
+
+        with open(additionalRegexLibPath,'r') as rl:
             self.additionalList = json.load(rl)['additional']
             for x in self.additionalList:
                 reg = self.additionalList[x]['regex']
+                funct = self.regexList[x]['code']
                 if reg is not '':
                     print("Init regex ", x, " ", reg)
-                    self.compiledRegexes.append(reg)
+                    if funct is not '':
+                        self.compiledRegexes.append([re.compile(reg), getattr(self.additionalCodeModule, funct)])
+                    else:
+                        self.compiledRegexes.append([re.compile(reg), None])
                 else:
                     print("Regex empty, not compiling")
 
-    def initRegexes(self, checkbuttonValues=None, regexLibPath='regexLib.json'):
+    def initRegexes(self, checkbuttonValues=None, regexLibPath='regexLib.json', codeLibPath='codeLib.py'):
+        """Throws key KeyError when the input file structure is incorrect"""
         print("\n-------------------------Initialising regexes-------------------------\n")
         if regexLibPath == 'regexLib.json':
             regexLibPath = os.path.join(os.path.dirname(__file__), regexLibPath)
+        self.regexPath = regexLibPath
+
+        if codeLibPath == 'codeLib.py':
+            loader = machinery.SourceFileLoader('codeLib', os.path.join(os.path.dirname(__file__),codeLibPath))
+            self.codeModule = loader.load_module()
+            #self.codeModule = importlib.import_module(codeLibPath[:-3], os.path.dirname(__file__))
+        else:
+            loader = machinery.SourceFileLoader(os.path.basename(codeLibPath[:-3]), codeLibPath)
+            self.codeModule = loader.load_module()
+        self.codeLibPath = codeLibPath
+
         with open(regexLibPath,'r') as rl:
             self.regexList = json.load(rl)['main']
             if checkbuttonValues is not None:
                 for x, y in checkbuttonValues.items():
                     if y.get() == 1:
                         reg = self.regexList[x]['regex']
+                        funct = self.regexList[x]['code']
                         if reg is not '':
                             print("Init regex ", x, " ", reg)
-                            self.compiledRegexes.append(reg)
+                            if funct is not '':
+                                self.compiledRegexes.append([re.compile(reg), getattr(self.codeModule, funct)])
+                            else:
+                                self.compiledRegexes.append([re.compile(reg), None])
                         else:
-                            print("Regex empty, not compiling")    
+                            print("Regex empty, not compiling")  
             else:
                 for x in self.regexList:
                     reg = self.regexList[x]['regex']
+                    funct = self.regexList[x]['code']
                     if reg is not '':
                         print("Init regex ", x, " ", reg)
-                        self.compiledRegexes.append(reg)
+                        if funct is not '':
+                            self.compiledRegexes.append([re.compile(reg), getattr(self.codeModule, funct)])
+                        else:
+                            self.compiledRegexes.append([re.compile(reg), None])
                     else:
                         print("Regex empty, not compiling")
 
 
-    def initOutput(self):
-        pass
+    def initOutput(self, path, backup=True):
+        print("\n--------------------------Initialising output-------------------------\n")
+        fullPath = os.path.join(path, "output.json")
+        self.outputFile = open(fullPath, "a")
+        self.outputPath = fullPath
+        if backup:
+            shutil.copy(self.inputPath, os.path.join(path, "input_backup.json"))
+
 
     def run(self):
+        x = 0
         while not self.cancel:
-            self.progressbar['value'] += 1
-            time.sleep(2)
+            content = self.parser.getNext()
+
+            if content is None:
+                break
+
+            x+=1
+            if x == self.parser.stepValue:
+                self.progressbar['value'] += 1
+                x=0
+            
+            for regex in self.compiledRegexes:
+                results = regex[0].findall(content['content'])
+                for result in results:
+                    if regex[1] is not None:
+                        if regex[1](result):
+                            print(result)
+                    else:
+                        print(result)
